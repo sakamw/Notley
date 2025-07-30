5d68081059e5e53576813fc71dc4b085a835e429
sakamw <shanmwangi2020@gmail.com>
2025-07-30 09:51:01 +0300

    add disabled

--- client/src/pages/auth/ProfilePage.tsx
+++ client/src/pages/auth/ProfilePage.tsx
@@ -77,7 +77,7 @@ const ProfilePage = () => {
       ) {
         setError(
           (err.response.data as { message?: string }).message ||
-            "Failed to update profile.",
+            "Failed to update profile."
         );
       } else {
         setError("Failed to update profile.");
@@ -111,7 +111,7 @@ const ProfilePage = () => {
       ) {
         setPasswordError(
           (err.response.data as { message?: string }).message ||
-            "Failed to update password.",
+            "Failed to update password."
         );
       } else {
         setPasswordError("Failed to update password.");
@@ -122,7 +122,7 @@ const ProfilePage = () => {
   const handleDeactivate = async () => {
     if (
       window.confirm(
-        "Are you sure you want to deactivate your account? This action cannot be undone.",
+        "Are you sure you want to deactivate your account? This action cannot be undone."
       )
     ) {
       try {
@@ -160,8 +160,8 @@ const ProfilePage = () => {
               {!user?.avatar && user?.firstName && user?.lastName
                 ? `${user.firstName[0]}${user.lastName[0]}`
                 : !user?.avatar && user?.firstName
-                  ? user.firstName[0]
-                  : "U"}
+                ? user.firstName[0]
+                : "U"}
             </Avatar>
             <Box>
               <Typography variant="h5" fontWeight={700} gutterBottom>
@@ -212,6 +212,7 @@ const ProfilePage = () => {
                 label="Email"
                 name="email"
                 value={form.email}
+                disabled
                 onChange={handleChange}
                 fullWidth
                 margin="normal"
